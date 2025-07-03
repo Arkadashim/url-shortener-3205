0fdfecf012994fa189906c7d36d3984d83cf2d43
arkadashiim <arkadashiim@mail.ru>
2025-07-03 18:09:21 +0300

    feat: typeorm module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlsModule } from './urls/urls.module';
 
 @Module({
-  imports: [UrlsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USERNAME', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', 'postgres'),
+        database: configService.get<string>('DB_NAME', 'url_shortener'),
+        entities: ['apps/backend/src/**/*.entity{.ts,.js}'],
+        synchronize: true, // Только для разработки!
+      }),
+    }),
+    UrlsModule,
+  ],
 })
 export class AppModule {}
